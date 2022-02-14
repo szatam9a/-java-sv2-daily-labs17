4b711d6f60d10012da9c87c0026d93b5360ae5d8
Szalay Tamás <szalaytamas90@gmail.com>
2022-02-14 16:08:07 +0100

    jdbc

---
+++ src/main/java/day01/Main.java
@@ -0,0 +1,26 @@
+package day01;
+
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        MariaDbDataSource dataSource = new MariaDbDataSource();
+        try {
+            dataSource.setUrl("jdbc:mariadb://localhost:3306/movies-actors?useUnicode=true");
+            dataSource.setUserName("root");
+            dataSource.setPassword("root");
+        } catch (SQLException e) {
+            throw new IllegalStateException("no sql connection");
+        }
+        try (Connection connection = dataSource.getConnection()) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("insert into actors(actor_name) values ('John Doe')");
+        } catch (SQLException e) {
+            throw new IllegalStateException("sql error");
+        }
+    }
+}
